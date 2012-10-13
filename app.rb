91d354a2fe395063b964061ec945b0d6d70be06e
Max Scheiber <keasbeynumb@gmail.com>
2012-10-13 19:12:56 -0400

    new and open routes

--- app.rb
+++ app.rb
@@ -4,20 +4,23 @@ require_relative 'config'
 require_relative 'models'
 
 enable :sessions
+$db_client = nil
 
 get '/' do
   redirect 'login' if !session['dropbox']
 
   session['dropbox'].get_access_token
-  db_client = DropboxClient.new(session['dropbox'], ACCESS_TYPE)
-  uid = db_client.account_info['uid']
+  $db_client = DropboxClient.new(session['dropbox'], ACCESS_TYPE) if !$db_client
+  uid = $db_client.account_info['uid']
   @user = User.first(dropbox_id: uid)
+  @new_user = false
 
   if !@user
     @user = User.create(
       dropbox_id: uid, 
       access_token: session['dropbox'].access_token)
-    db_client.put_file('/Public/DBIDE/Project1/index.html', 'Hello World')
+    $db_client.put_file('/Public/DBIDE/Project1/index.html', 'Hello World')
+    @new_user = true
   end
 
   @js = ['/js/lib/jquery.js', '/js/lib/underscore.js', '/js/lib/backbone.js', '/js/lib/ace/ace.js']
@@ -28,12 +31,28 @@ get '/login' do
   db_session = DropboxSession.new(APP_KEY, APP_SECRET)
   session['dropbox'] = db_session
   db_session.get_request_token
-  @authorize_url = db_session.get_authorize_url('http://localhost:9393')
+  @authorize_url = db_session.get_authorize_url(SITE_URL)
 
   erb :login
 end
 
+post '/new' do
+  return if !$db_client
+
+  begin
+    name = params[:name] || "Unnamed Project"
+    db_client.file_create_folder('/Public/DBIDE/#{name}')
+    return true
+  rescue DropboxError
+    return false
+  end
+end
+
 post '/open' do
+  return if !$db_client
+  name = params[:path]
+  content_type :json
+  return db_client.get_file_and_metadata('/Public/DBIDE/#{path}').to_json
 end
 
 post '/save' do
