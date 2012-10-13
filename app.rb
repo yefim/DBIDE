85c913a11275ebafb36d9c01e75b2a59ac1a3dab
Max Scheiber <keasbeynumb@gmail.com>
2012-10-13 16:36:33 -0400

    basic login

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require 'sinatra'
 require 'dropbox_sdk'
 require_relative 'config'
 
+enable :sessions
+
 get '/' do
   erb :index
 end
 
 get '/login' do
+  session = DropboxSession.new(APP_KEY, APP_SECRET)
+  session.get_request_token
+  @authorize_url = session.get_authorize_url('http://localhost:9393')
   erb :login
 end
 
