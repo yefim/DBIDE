02123647d6b442fb5cdfdd20d3ec374aed7efb7a
Max Scheiber <keasbeynumb@gmail.com>
2012-10-13 16:10:58 -0400

    config.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'dropbox_sdk'
 require_relative 'config'
 
 get '/' do
---
+++ config.rb
@@ -0,0 +1,3 @@
+APP_KEY = 'jcgzyr4kzg0n48n'
+APP_SECRET = '6gfxpi0zjpb9nne'
+ACCESS_TYPE = :app_folder # we want our own DBIDE folder
