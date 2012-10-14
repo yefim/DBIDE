4ff8c18ddf1b138a8b85ff7c35508fa4bbefc02a
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-10-13 21:38:18 -0400

    Added more js_files

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ get '/' do
     @new_user = true
   end
 
-  @projects = $db_client.metadata("#{ROOT}", 25000, true, nil, nil, true).fetch("contents")
+  @projects = $db_client.metadata("#{ROOT}", 25000, true, nil, nil, false).fetch("contents")
 
   @js = ['lib/jquery', 'lib/underscore', 'lib/backbone', 'lib/ace/ace', 'dbide', 'models/file', 'views/main_view']
   erb :index
